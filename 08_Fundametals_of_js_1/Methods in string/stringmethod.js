662620dc26c365eee9a9b85d925478883ce6b8fb
varun pandian <60826092+varunpandian-developer@users.noreply.github.com>
2024-08-05 19:17:44 +0530

    Learning on progress

--- 08_Fundametals_of_js_1/Methods in string/stringmethod.js
+++ 08_Fundametals_of_js_1/Methods in string/stringmethod.js
@@ -1,22 +1,41 @@
-//String wraper object 
+// String wrapper object 
 let str = "javascript iS wonderfull";
+let str2 = "it is dynamically typed"; // Corrected spelling
 
-let str2 = "it is dynamicically typed"
+// Prototype of the string object
+console.log(str.__proto__); // Logs the prototype of the String object
 
-console.log(str.__proto__);
-console.log(str.charAt(0))
-console.log(str.charAt(20))
+// Character at specific positions
+console.log(str.charAt(0)); // Logs 'j'
+console.log(str.charAt(20)); // Logs 'l'
 
-console.log(str.concat(str2))
-console.log(str2.concat(str))
+// Concatenate strings
+console.log(str.concat(str2)); // Concatenates and logs "javascript iS wonderfullit is dynamically typed"
+console.log(str2.concat(str)); // Concatenates and logs "it is dynamically typedjavascript iS wonderfull"
 
-console.log(str.length)
-console.log(str.indexOf("s"))
-console.log(str.indexOf("S"))
-console.log(str.indexOf("r",8))
-console.log(str.lastIndexOf(l))
+// String length
+console.log(str.length); // Logs 23
 
+// Index of first occurrence of a character
+console.log(str.indexOf("s")); // Logs 4
 
+// Index of first occurrence of 'l'
+console.log(str.indexOf("l")); // Logs 21
 
+// Index of first occurrence of 'r' after index 7
+console.log(str.indexOf("r", 7)); // Logs 9
 
+// Index of last occurrence of 's' in str2
+console.log(str2.lastIndexOf('s')); // Logs 5 (because the last 's' is at index 5 in str2)
 
+// Character at index 15
+let message = "hello world hello world hello world";
+console.log(message.charAt(15)); // Logs a space ' '
+
+// Last index of 'l' in message
+console.log(message.lastIndexOf('l')); // Logs 35
+
+const sentence = " Hello world";
+const charToFind='o';
+const indexofchar = sentence.indexOf(charToFind).toUpperCase().charAt(1);
+console.log(indexofchar)
---
+++ js 2 assignment operators in JS/Opeartors.js
@@ -0,0 +1,32 @@
+/** Operators in JS */
+
+// 1. Assignment Operator(=)
+let a = 10;
+let b = a;
+let c = (a = 20);
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// 2. Arithematic Opeartors (+, -, *, /, %)
+// console.log(10 + 2);
+// console.log(10 - 2);
+// console.log(10 * 2);
+// console.log(10 / 2);
+// console.log(11 % 2);
+
+// 3. More Arithematic Operators (++, --, +, -, **)
+let i = 2;
+//i = i + 1;
+//i++;
+
+// console.log(++i);
+//console.log(i ** 3);
+
+// 4. Augmented Assignment Opeartor
+// (+=, -=,*=, /=,.....)
+//i = i+3;
+i += 3;
+
+console.log(i);
