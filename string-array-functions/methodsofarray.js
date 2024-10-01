c4e429333328e420fc1836b8ea2a0f3369b02e64
varun pandian <60826092+varunpandian-developer@users.noreply.github.com>
2024-10-01 23:35:32 +0530

    method of array

---
+++ string-array-functions/methodsofarray.js
@@ -0,0 +1,13 @@
+const ipl = ['Csk','rcb','srh'];
+ipl.shift()
+const pusharray=ipl.push('lsg')
+const unshiftarray=ipl.unshift('csg')
+
+const poparray = ipl.pop()
+const shiftarray = ipl.shift()
+
+console.log(pusharray)
+console.log(unshiftarray)
+console.log(ipl)
+console.log(poparray)
+console.log(shiftarray)
