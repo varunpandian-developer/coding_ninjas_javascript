2fc15e0d2ed42757b5ffbc19f04d485289a611f4
varun pandian <60826092+varunpandian-developer@users.noreply.github.com>
2024-10-07 20:32:59 +0530

    traverse string

---
+++ string-array-functions/traversingstring.js
@@ -0,0 +1,15 @@
+// let str = "javascript is an amazing language"
+
+// for( let i =0 ; i<str.length; i++)
+//     {
+
+//     console.log(str.charAt(i));
+// }
+
+//find the occurnce of the character in string
+
+ let str = "javascript is an amazing language"
+
+ for(let i =0; i<str.length; i++){
+    
+ }
