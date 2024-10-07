d0d5dd2c669091d8958a938a567597dadca48586
varun pandian <60826092+varunpandian-developer@users.noreply.github.com>
2024-10-07 20:37:34 +0530

    find the character count

--- string-array-functions/traversingstring.js
+++ string-array-functions/traversingstring.js
@@ -9,7 +9,17 @@
 //find the occurnce of the character in string
 
  let str = "javascript is an amazing language"
+ let char= "a"
+ let charcount=0
 
  for(let i =0; i<str.length; i++){
     
+    if(str[i] === char)
+    {
+        charcount++
+
+    }
+    
  }
+
+ console.log(charcount);
