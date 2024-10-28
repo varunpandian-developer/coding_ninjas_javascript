3a3cd4d083df0f6ff162763903ab2327a09202a0
varun pandian <60826092+varunpandian-developer@users.noreply.github.com>
2024-10-28 07:53:59 +0530

    nothing changed just enter

--- week2_working_on_js/function-in-js/reducefuntion.js
+++ week2_working_on_js/function-in-js/reducefuntion.js
@@ -6,3 +6,12 @@ const sum = inputs.reduce((total,sum)=>{
 },0)
 
 console.log(sum)
+
+//sum only even
+const inputs = [2, 3, 4, 5, 6];
+const sum = inputs.reduce((total, num) => 
+    {
+        return total + (num % 2 === 0 ? num : 0)
+    }, 0);
+
+console.log(sum); // Output: 12 (2 + 4 + 6)
